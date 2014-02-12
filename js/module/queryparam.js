60ce90a69c9979c2696830360bfba02d939f44bc
shy2850 <shy2850@163.com>
2014-02-12 17:03:21 +0800

    queryparam修改

--- js/module/queryparam.js
+++ js/module/queryparam.js
@@ -6,22 +6,20 @@ define(function(require, exports, module) {
     **/
 	return function(key){
 	    var search = queryParam.search ||  (function(w){
-	        var m = {},ar = w.location.search;
+	        var m = {},ar = w.location.search, t;
 	        if(ar){
-	            var arr = ar.substring(1).split('&');
-	            for(var i = 0; i < arr.length; i++){
-	                var t = arr[i].split('=');
-	                switch( typeof m[t[0]] ){
+	        	ar.substring(1).replace( /([^&=]+)[=]([^&=]*)[&=\b]?/g,function(macth,k,v){
+	        		switch( typeof m[k] ){
 	                	case 'string': //如果是重复参数，包装成数组
-	                		m[t[0]] = new Array( m[t[0]], t[1] );
+	                		m[k] = new Array( m[k], v );
 	                		break;
 	                	case 'object': //如果已经是数组，push
-	                		m[t[0]].push( t[1] );
+	                		m[k].push( v );
 	                		break; 
 	                	default: 	//第一次需要设置为字符串
-	                 		m[t[0]] = t[1];
+	                 		m[k] = v;
 	                }
-	            }
+	        	} );
 	        }
 	        queryParam.search = m;
 	        return m;
