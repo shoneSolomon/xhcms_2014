68a61fbb43af6e80d28db94140fb74332326182a
shy2850 <shy2850@163.com>
2014-02-25 08:44:17 +0800

    SVN冲突解决失败，重新提交

--- frame/js/frame.js
+++ frame/js/frame.js
@@ -1,18 +1,32 @@
 (function($){
 window.isLocal = !( location.host && /news\.cn/.test(location.host) );
 
-var conf = 	{
-				'msg':{
-					navLine: '<a href="javascript:void(0);">站内信</a>',
-					withNav: false
-				}
-			};
+var conf = 	
+{
+		'msg':{
+		navLine: '<a href="javascript:void(0);">站内信</a>',
+		URL : (isLocal ? {
+				groups : "json/groups.json",
+				send_lists: "json/msgList.json",
+				receive_lists: "json/msgList.json",
+				recycle_lists: "json/msgList.json"
+			} : {
+				groups : "json/groups.json",
+				send_lists: "json/msgList.json",
+				receive_lists: "json/msgList.json",
+				recycle_lists: "json/msgList.json"
+			})
+	}
+};
 
 $.initFrame = function(key){
-	var _this;
-	if( _this = conf[key] ){
-		
+	var _this = conf[key];
+	if( _this ){
+		if( window.top != window ){
+			$(".mainHead",top.document).html( _this.navLine );
+		}
 	}
+	return _this;
 };
 
 
