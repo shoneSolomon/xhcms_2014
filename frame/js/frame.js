5bb460dd07e0859955c9ca8266a75ceffb584890
shy2850 <shy2850@163.com>
2014-03-12 16:50:52 +0800

    msg.html问题修复

--- frame/js/frame.js
+++ frame/js/frame.js
@@ -7,11 +7,13 @@ var conf =
 		navLine: '<a href="javascript:void(0);">站内信</a>',
 		URL : (isLocal ? {
 				groups : "json/groups.json",
+				submit_form: "json/success.json",
 				send_lists: "json/msgList.json",
 				receive_lists: "json/msgList.json",
 				recycle_lists: "json/msgList.json"
 			} : {
 				groups : "json/groups.json",
+				submit_form: "json/success.json",
 				send_lists: "/web/admin/msg/user/send/list.json?",
 				receive_lists: "/web/admin/msg/user/all/list.json?",
 				recycle_lists: "/web/admin/msg/user/recycle/list.json?"
