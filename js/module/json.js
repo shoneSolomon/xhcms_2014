8227c5e181e68ed9521a767167530078378e9035
shy2850 <shy2850@163.com>
2014-04-18 09:26:28 +0800

    json模块修改，对于循环引用对象的stringify抛出异常

--- js/module/json.js
+++ js/module/json.js
@@ -2,6 +2,8 @@ define(function(require, exports, module) {
 	function isArray(obj) { 
 		return ({}).toString.call(obj) === '[object Array]'; 
 	}
+	var objs = [];
+
 	var JSON = window.JSON || {
 		parse : function(jsonStr){
 			return new Function("return ("+jsonStr+")").call(this);
@@ -9,6 +11,11 @@ define(function(require, exports, module) {
 		stringify : function(o){
 			var tp = (null === o) ? 'undefined' : typeof o,
 				callee = arguments.callee;
+			
+			if( this.stringify === callee ){	//如果是第一次的非递归调用, 初始化objs
+				objs = [];
+			}
+
 			switch(tp){
 				case 'undefined':
 				case 'number':
@@ -16,6 +23,11 @@ define(function(require, exports, module) {
 				case 'string': return '"' + o.replace(/\\/g,'\\\\').replace(/"/g,'\"') + '"'; 
 				case 'function': return '"[object Function]"';
 				case 'object':
+					// 跟原生的JSON.stringify一样,对于循环调用的JSON抛出异常
+					for (var i = 0; i < objs.length; i++) {		
+						if( objs[i] === o ) throw new Error("Converting circular structure to JSON");
+					}
+					objs.push(o);
 				default : 
 					if( isArray(o) ){
 						return (function(o){
@@ -59,7 +71,7 @@ define(function(require, exports, module) {
 			if(with_ref){
 				(function(o){
 					for(var k in o){
-						if(typeof o[k].$ref === 'string'){
+						if(o[k] && typeof o[k].$ref === 'string'){
 							o[k] = getRef(r,o[k].$ref.replace(/\[(\d+)\]/g,'.$1')); //有数组格式的数据要转化一下。
 						}else if(typeof o[k] === 'object'){
 							arguments.callee(o[k]);
